61a83ceefcf288f4507ba7fecd46cb28e808c8bc
Vanness Aurelius Gunawan <vaurelius03@student.ciputra.ac.id>
2026-05-15 01:28:45 +0700

    initial

---
+++ kognite/kognite/kogniteApp.swift
@@ -0,0 +1,17 @@
+//
+//  kogniteApp.swift
+//  kognite
+//
+//  Created by Vanness Aurelius Gunawan on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct kogniteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
