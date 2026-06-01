39cd5d5bc9007b05025f6e34f897f53e5e604a1d
Vanness Aurelius Gunawan <vaurelius03@student.ciputra.ac.id>
2026-06-01 15:09:02 +0700

    create model user

---
+++ kognite/kognite/Model/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  kognite-se
+//
+//  Created by Vanness Aurelius Gunawan on 12/05/26.
+//
+
+import Foundation
+import FirebaseFirestore
+
+// Merepresentasikan entitas profil pengguna untuk menyimpan identitas dasar dan akumulasi statistik produktivitas
+struct User: Identifiable, Codable {
+    @DocumentID var id: String?
+    var username: String
+    var email: String
+    var currentStreak: Int
+    var totalCompletedTasks: Int?
+}
